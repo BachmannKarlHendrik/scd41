35d206a6cca29d9a8fb36fa838acbfac9e317ad7
KarlHendrikBachmann <71638019+KarlHendrikBachmann@users.noreply.github.com>
2022-01-07 16:32:36 +0200

    Changed MQTT Library

--- src/main.cpp
+++ src/main.cpp
@@ -1,120 +1,157 @@
-#include <WiFi.h>
-#include <PubSubClient.h>
+#include <Arduino.h>
 #include <Wire.h>
-#include "credentials.h"
+#include <WiFi.h>
+extern "C" {
+	#include "freertos/FreeRTOS.h"
+	#include "freertos/timers.h"
+}
+#include <AsyncMqttClient.h>
+#include <credentials.h>
+
 
 //Wifi info and debugging led
 int LED_BUILTIN = 2;
 
-const char* ssid     = WIFINAME;
-const char* password = WIFIPASS;
-const char* url = URL; //Endpoint server
-const char* tenant = TENANT;
-const char* passwordiot = IOTPASS;
-String clientId = "KarliESP-SCD41-1";
+String clientId = "KarliESP-SCD41-2";
 String command = "";
+String TOPIC = "s/us";
 unsigned long lastMsg = 0;
 
+AsyncMqttClient mqttClient;
+TimerHandle_t mqttReconnectTimer;
+TimerHandle_t wifiReconnectTimer;
 
-WiFiClient wifiClient;
-PubSubClient mqttClient(wifiClient);
-
-// SCD4x
 const int16_t SCD_ADDRESS = 0x62;
 
-// Helper functions declarations
-void reconnect();
+void connectToWifi() {
+  Serial.println("Connecting to Wi-Fi...");
+  WiFi.begin(WIFINAME, WIFIPASS);
+}
 
+void connectToMqtt() {
+  Serial.println("Connecting to MQTT...");
+  mqttClient.connect();
+  command = "100,"+clientId+",c8y_MQTTdevice";
+  mqttClient.publish(TOPIC.c_str(), 0, false, command.c_str());
+}
+
+void WiFiEvent(WiFiEvent_t event) {
+    Serial.printf("[WiFi-event] event: %d\n", event);
+    switch(event) {
+    case SYSTEM_EVENT_STA_GOT_IP:
+        Serial.println("WiFi connected");
+        Serial.println("IP address: ");
+        Serial.println(WiFi.localIP());
+        connectToMqtt();
+        break;
+    case SYSTEM_EVENT_STA_DISCONNECTED:
+        Serial.println("WiFi lost connection");
+        xTimerStop(mqttReconnectTimer, 0); // ensure we don't reconnect to MQTT while reconnecting to Wi-Fi
+        xTimerStart(wifiReconnectTimer, 0);
+        break;
+    default:
+      Serial.println("Unhandled wifi event just happened!");
+    }
+}
+
+void onMqttConnect(bool sessionPresent) {
+  digitalWrite(LED_BUILTIN, LOW);
+  Serial.println("Connected to MQTT.");
+  Serial.print("Session present: ");
+  Serial.println(sessionPresent);
+}
+
+void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
+  digitalWrite(LED_BUILTIN, HIGH);
+  Serial.println("Disconnected from MQTT.");
+
+  if (WiFi.isConnected()) {
+    xTimerStart(mqttReconnectTimer, 0);
+  }
+}
+
+void onMqttSubscribe(uint16_t packetId, uint8_t qos) {
+  Serial.println("Subscribe acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+  Serial.print("  qos: ");
+  Serial.println(qos);
+}
+
+void onMqttUnsubscribe(uint16_t packetId) {
+  Serial.println("Unsubscribe acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+}
+
+void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total) {
+  Serial.println("Publish received.");
+  Serial.print("  topic: ");
+  Serial.println(topic);
+  Serial.print("  qos: ");
+  Serial.println(properties.qos);
+  Serial.print("  dup: ");
+  Serial.println(properties.dup);
+  Serial.print("  retain: ");
+  Serial.println(properties.retain);
+  Serial.print("  len: ");
+  Serial.println(len);
+  Serial.print("  index: ");
+  Serial.println(index);
+  Serial.print("  total: ");
+  Serial.println(total);
+}
+
+void onMqttPublish(uint16_t packetId) {
+  Serial.println("Publish acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+}
 
 void setup() {
+  //Start serial and LED
   Serial.begin(115200);
   while(!Serial);
-  //=========================================
-  // We start by connecting to a WiFi network
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-
-  WiFi.begin(ssid, password);
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
-  while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.print(".");
-  }
-  Serial.println("WiFi connected");
-  //=========================================
-  //MQTT
-  mqttClient.setServer(url, 1883);
-  reconnect();
-  //=========================================
-  digitalWrite(LED_BUILTIN, LOW);
 
-  //=========================================
-  // output format
-  Serial.println("CO2(ppm)\tTemperature(degC)\tRelativeHumidity(percent)");
-  
-  // init I2C
+  Serial.println();
+  Serial.println();
+
+  //Sensor setup
   Wire.begin();
 
-  // wait until sensors are ready, > 1000 ms according to datasheet
-  delay(1000);
-  
   // start scd measurement in periodic mode, will update every 5 s
   Wire.beginTransmission(SCD_ADDRESS);
   Wire.write(0x21);
   Wire.write(0xb1);
   Wire.endTransmission();
+  lastMsg = millis(); // Make sure that there is no measuring done in 5 seconds.
 
-  // wait for first measurement to be finished
-  delay(5000);
-}
+  //Create MQTT timers
+  mqttReconnectTimer = xTimerCreate("mqttTimer", pdMS_TO_TICKS(2000), pdFALSE, (void*)0, reinterpret_cast<TimerCallbackFunction_t>(connectToMqtt));
+  wifiReconnectTimer = xTimerCreate("wifiTimer", pdMS_TO_TICKS(2000), pdFALSE, (void*)0, reinterpret_cast<TimerCallbackFunction_t>(connectToWifi));
 
-void reconnect() {
-  // Loop until we're reconnected
-  while (!mqttClient.connected()) {
-    digitalWrite(LED_BUILTIN, HIGH);
-    Serial.print("Attempting MQTT connection...");
-    // Attempt to connect
-    if (mqttClient.connect(clientId.c_str(),tenant,passwordiot)) {
-      Serial.println("Connected");
-      String command = "100,"+clientId+",c8y_MQTTdevice";
-      mqttClient.publish("s/us", (char*) command.c_str());
-      digitalWrite(LED_BUILTIN, LOW);
-      // Connected - do something useful - subscribe to topics, publish messages, etc.
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(mqttClient.state());
-      // Wait 5 seconds before retrying
-      Serial.println("Disconnecting wifi");
-      WiFi.disconnect();
-      Serial.println("Reconnecting wifi in 5 seconds");
-      Serial.print("Connecting to ");
-      Serial.println(ssid);
-      delay(5000);
-      WiFi.reconnect();
-      int wifiCounter = 0;
-      while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
-        wifiCounter +=1;
-        if(wifiCounter > 30) {
-          Serial.println("Wifi reconnect timed out. Restarting ESP.");
-          ESP.restart();
-        }
-      }
-    }
-  }
+  //Create WIFI state listener
+  WiFi.onEvent(WiFiEvent);
+
+  //Create MQTT state methods
+  mqttClient.onConnect(onMqttConnect);
+  mqttClient.onDisconnect(onMqttDisconnect);
+  mqttClient.onSubscribe(onMqttSubscribe);
+  mqttClient.onUnsubscribe(onMqttUnsubscribe);
+  mqttClient.onMessage(onMqttMessage);
+  mqttClient.onPublish(onMqttPublish);
+  mqttClient.setServer(URL, 1883);
+  mqttClient.setCredentials(TENANT,IOTPASS);
+  mqttClient.setClientId(clientId.c_str());
+
+  //Start connection
+  connectToWifi();
 }
 
 void loop() {
-  if (!mqttClient.connected()) {
-    reconnect();
-  }
-
-  mqttClient.loop();
-
-    unsigned long now = millis();
+  unsigned long now = millis();
   if (now - lastMsg > 5000) { // Wait 5 seconds between every request
     lastMsg = now;
   
@@ -154,11 +191,10 @@ void loop() {
     Serial.println();
 
     command = "200,co2Measurement,particles per million,"+String(co2)+",ppm";
-    mqttClient.publish("s/us", (char*) command.c_str());
+    mqttClient.publish(TOPIC.c_str(), 0, false, command.c_str());
     command = "211,"+String(temperature);
-    mqttClient.publish("s/us", (char*) command.c_str());
+    mqttClient.publish(TOPIC.c_str(), 0, false, command.c_str());
     command = "200,humidityMeasurement,percent,"+String(humidity)+",%";
-    mqttClient.publish("s/us", (char*) command.c_str());
-  
+    mqttClient.publish(TOPIC.c_str(), 0, false, command.c_str());
   }
 }
